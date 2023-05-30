6a3a998512be3d3a98a826c61a6f187089ad811a
Sveta <svetik.2007@list.ru>
2023-05-30 23:32:59 +0300

    HW6

---
+++ HW_06/task41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int m = 5;
+int res = 0;
+
+int Prompt( int n )
+{
+	int countPositive = 0;
+	int curPrompt;
+
+	Console.WriteLine($"Введите { n } чисел");
+
+	for (int i = 0; i < n; i++)
+	{
+		curPrompt = int.Parse(Console.ReadLine());
+		if ( curPrompt > 0 ) countPositive++;
+	}
+
+	return countPositive;
+}
+
+res = Prompt( m );
+
+Console.WriteLine($"Пользователь ввёл { res } положительных чисел.");
---
+++ HW_06/task43/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int Prompt( string whatWePrompt )
+{
+	int curPrompt;
+
+	Console.WriteLine($"Введите { whatWePrompt }");
+
+	curPrompt = int.Parse(Console.ReadLine());
+
+	return curPrompt;
+}
+
+double k1 = Prompt("k1");
+double b1 = Prompt("b1");
+double k2 = Prompt("k2");
+double b2 = Prompt("b2");
+
+double x,y;
+
+if ( k1 == k2 )
+{
+	Console.WriteLine("Прямые параллельны");
+}
+else
+{
+	x = ( b2 - b1 ) / ( k1 - k2 );
+	y = k1 * x + b1;
+	Console.WriteLine($"Координаты точки пересечения ({ x }; { y })");
+}
+
+
